adb7ea7b154dc4ac4dde5ee18eb8d299d2258d4e
Per Ottosson (ext) <otpe20pu@student.ju.se>
2022-10-10 13:18:30 +0200

    implements loading library and calling init function

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
 #include "platform/platform.h"
+#include "sbx-args.h"
+#include "sbxn.h"
+#define STR(X) #X
 
 int main(int argc, const char **argv) {
+    SbxArgs args = { 0 };
+    SbxArgsStatus argsStatus = sbx_args_parse(argc, argv, &args);
+    if(argsStatus != SBX_ARGS_VALID) {
+        return argsStatus;
+    }
 
+    LibHandle lib = shared_lib_load(args.libFileName);
+    sbx_init *initFunc = shared_lib_get_proc(lib, STR(SBX_INIT_NAME));
+    initFunc();
 }
